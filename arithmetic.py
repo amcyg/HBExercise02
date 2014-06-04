03979f7dcb25cce20cd89df8f947930ecaf00787
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 12:02:37 -0700

    Added comments to explain usage

--- arithmetic.py
+++ arithmetic.py
@@ -10,14 +10,17 @@ def multiply(num1, num2):
 def divide(num1, num2):
     return num1/num2
 
+#to use, type square num1
 def square(num1):
     return num1*num1
 
+#to use, type cube num1
 def cube(num1):
     return num1*num1*num1
 
+#to use, type pow num1 num2
 def power(num1, num2):
     return num1**num2
-
+#to use, type mod num1 num2
 def mod(num1, num2):
     return num1%num2
